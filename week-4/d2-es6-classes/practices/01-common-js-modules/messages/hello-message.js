75bd1410f2863fded4d465a12e15e4f0047b7eff
Willard Marett <will.j.marett@gmail.com>
2022-06-01 10:58:44 -0500

    Importing and Exporting

--- week-4/d2-es6-classes/practices/01-common-js-modules/messages/hello-message.js
+++ week-4/d2-es6-classes/practices/01-common-js-modules/messages/hello-message.js
@@ -1,1 +1,8 @@
 const helloMessage = "Greetings! Let us begin!";
+
+module.exports = helloMessage;
+// module.exports = { helloMessage }
+/*
+there are different styles to use to import and export, but
+the styles need to match across files.
+*/
