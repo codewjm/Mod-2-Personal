dfd7c6edd6b0a91819ba10733ba18d217a33e52d
Willard Marett <will.j.marett@gmail.com>
2022-06-18 23:15:56 -0500

    insertion-sorts

--- week-6/d1-sorting-bubble-insertion-selection/notes/insertion.js
+++ week-6/d1-sorting-bubble-insertion-selection/notes/insertion.js
@@ -9,7 +9,7 @@ Steps of Insertion Sort:
 1. If it is the first element, it is already sorted. return 1;
 2. Pick next element
 3. Compare with all elements in the sorted sub-list
-4. Shift all the elements in the sorted sub-list that is greater than the value
+4. Shift all the elements in the sorted sub-list that are greater than the value
    to be sorted
 5. Insert the value
 6. Repeat until list is sorted
@@ -40,3 +40,8 @@ function insertionSort(arr) {
   }
   return arr;
 }
+
+let nums = [5, 3, 4, 7, 6];
+
+insertionSort(nums)
+console.log(insertionSort(nums))
